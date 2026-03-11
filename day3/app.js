bf67b73e6e639ef4efca76d818421a5b8f77b0f9
sathekartik57 <sathekartik57@gmail.com>
2026-03-11 15:34:31 +0530

    day3 task completed

---
+++ day3/app.js
@@ -0,0 +1,6 @@
+console.log("welcome to backend development internship");
+console.log("learning node.js basics");
+console.log("day 3 task completed successfully");
+console.log("my name is kartik");
+console.log("im currenty doing my masters in computer application");
+console.log("node.js is interesting");
